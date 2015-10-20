303b1b2b55310c78c6418b351f78910387dcaa88
Markus Felten <Markus.Felten@gmx.de>
2015-10-20 22:18:26 +0200

    initial checkin

---
+++ index.js
@@ -0,0 +1,132 @@
+function checkStepStatic(aStep, additionalAsserts) {
+  describe('manager', function () {
+    it('present', function () {
+      assert.equal(aStep.manager, manager);
+    });
+  });
+
+  describe('state', function () {
+    it('stopped', function () {
+      assert.equal(aStep.state, 'stopped');
+    });
+  });
+
+  if (additionalAsserts) {
+    additionalAsserts();
+  }
+}
+
+function checkStepLivecycle(aStep, additionalAsserts) {
+  it('can be stopped in stopped state', function (done) {
+    aStep.stop().then(
+      function (step) {
+        try {
+          assert.equal(aStep, step);
+          assert.equal(aStep.state, 'stopped');
+          done();
+        } catch (e) {
+          done(e);
+        }
+      }, done);
+    //assert.equal(aStep.state, 'failed');
+  });
+
+  it('can be started', function (done) {
+    aStep.start().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'running');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'running');
+        }
+        setTimeout(done, 10); // wait for some requests to pass through
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+    assert.equal(aStep.state, 'starting');
+  });
+
+  it('can be started again', function (done) {
+    aStep.start().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'running');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'running');
+        }
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+    assert.equal(aStep.state, 'running');
+  });
+
+  it('and then stopped', function (done) {
+    aStep.stop().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'stopped');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'stopped');
+        }
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+    assert.equal(aStep.state, 'stopping');
+  });
+
+  it('and then stopped again', function (done) {
+    aStep.stop().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'stopped');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'stopped');
+        }
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+    assert.equal(aStep.state, 'stopped');
+  });
+
+
+  it('can be started while starting', function (done) {
+    aStep.start().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'running');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'running');
+        }
+        //done();
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+
+    aStep.start().then(function (step) {
+      try {
+        assert.equal(aStep, step);
+        assert.equal(aStep.state, 'running');
+        if (additionalAsserts) {
+          additionalAsserts(aStep, 'running');
+        }
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }, done);
+
+    assert.equal(aStep.state, 'starting');
+  });
+}
+
+
+exports.checkStepStatic = checkStepStatic;
+exports.checkStepLivecycle = checkStepLivecycle;
