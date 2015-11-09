0a6e60af04bb1d18f2d4604c54bae4e9ce3a2db2
Torsten Link <torsten.link@db.com>
2015-11-09 23:54:47 +0100

    add the manager mock

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 "use strict";
 
 const StepTest = require('./test/step_test');
+const managerMock = require('./lib/manager-mock');
 
 module.exports.checkStepStatic = StepTest.checkStepStatic;
 module.exports.checkStepLivecycle = StepTest.checkStepLivecycle;
+
+// Used for test. Each step nest needs a manager. This is a mockup used for testing steps
+module.exports.managerMock = managerMock;
