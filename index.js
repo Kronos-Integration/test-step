3910d9f0e9054e857de2f4a62516962536fe4d1a
Markus Felten <Markus.Felten@gmx.de>
2016-02-02 13:37:17 +0100

    feat: user service 2.0
    BREAKING CHANGE: manager mock is gone

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 "use strict";
 
 const StepTest = require('./tests/step_test');
-const managerMock = require('./lib/manager-mock');
 
 module.exports.checkStepStatic = StepTest.checkStepStatic;
 module.exports.checkStepLivecycle = StepTest.checkStepLivecycle;
-
-// Used for test. Each step nest needs a manager. This is a mockup used for testing steps
-module.exports.managerMock = managerMock;
