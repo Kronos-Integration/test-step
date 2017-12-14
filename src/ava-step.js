c7e7dc9a29ea56b9ebc53c2f37c5b893a958c09b
Markus Felten <Markus.Felten@gmx.de>
2017-12-14 11:04:58 +0100

    wip

--- src/ava-step.js
+++ src/ava-step.js
@@ -5,12 +5,11 @@ export async function stepTestStatic(
   Factory,
   config,
   type,
-  manager,
+  owner,
   additionalAsserts = dummy
 ) {
-  const instance = new Factory(config, manager);
+  const instance = new Factory(config, owner);
 
-  t.is(instance.manager, manager);
   t.is(instance.state, 'stopped');
   t.is(instance.type, type);
 
@@ -21,8 +20,8 @@ export async function stepTestStatic(
 
 export async function checkStepLivecycle(
   t,
-  manager,
-  aStep,
+  owner,
+  step,
   additionalAsserts = dummy
 ) {
   const livecycle = {
@@ -36,134 +35,16 @@ export async function checkStepLivecycle(
     });
   };
 
-  manager.addListener('stepStateChanged', stepStateChangedListener);
+  owner.addListener('stepStateChanged', stepStateChangedListener);
 
-  if (!additionalAsserts)
-    additionalAsserts = (step, state, livecycle, done) => {
-      done();
-    };
+  await step.stop();
 
-  it('can be stopped in stopped state', done => {
-    aStep.stop().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'stopped');
-        additionalAsserts(aStep, 'stopped', livecycle, done);
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-  });
+  t.is(step.state, 'stopped');
+  await additionalAsserts(t, step, 'stopped', livecycle);
 
-  it('can be started', done => {
-    aStep.start().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'running');
-        additionalAsserts(aStep, 'running', livecycle, e => {
-          setTimeout(function() {
-            done(e);
-          }, 100); // wait for some requests to pass through
-        });
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-    assert.equal(aStep.state, 'starting');
-  });
+  await step.start();
 
-  it('can be started again', done => {
-    aStep.start().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'running');
-        additionalAsserts(aStep, 'running', livecycle, done);
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-    assert.equal(aStep.state, 'running');
-  });
+  t.is(step.state, 'running');
 
-  it('and then stopped', done => {
-    aStep.stop().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'stopped');
-        additionalAsserts(aStep, 'stopped', livecycle, done);
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-    assert.equal(aStep.state, 'stopping');
-  });
-
-  it('and then stopped again', done => {
-    aStep.stop().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'stopped');
-        additionalAsserts(aStep, 'stopped', livecycle, done);
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-    assert.equal(aStep.state, 'stopped');
-  });
-
-  it('can be started while starting', done => {
-    aStep.start().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'running');
-        additionalAsserts(aStep, 'running', livecycle, function() {});
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-
-    aStep.start().then(step => {
-      try {
-        assert.equal(aStep, step);
-        assert.equal(aStep.state, 'running');
-        additionalAsserts(aStep, 'running', livecycle, done);
-      } catch (e) {
-        done(e);
-      }
-    }, done);
-
-    assert.equal(aStep.state, 'starting');
-  });
-
-  it('can be stopped while starting', done => {
-    aStep.stop().then(function() {
-      aStep.start();
-      assert.equal(aStep.state, 'starting');
-      aStep.stop().then(step => {
-        try {
-          assert.equal(aStep, step);
-          assert.equal(aStep.state, 'stopped');
-          done();
-        } catch (e) {
-          done(e);
-        }
-      }, done);
-    });
-  });
-
-  /*
-		it('can be removed', function (done) {
-			aStep.stop().then(aStep.remove().then(function (step) {
-				try {
-					assert.equal(aStep, step);
-					assert.equal(aStep.state, 'removed');
-					additionalAsserts(aStep, 'removed', livecycle, done);
-				} catch (e) {
-					done(e);
-				}
-			}, done));
-});
-*/
-
-  manager.removeListener('stepStateChanged', stepStateChangedListener);
+  owner.removeListener('stepStateChanged', stepStateChangedListener);
 }
